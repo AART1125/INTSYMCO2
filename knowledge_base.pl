b55aca9120809d9f69a5d95b6bf5c8e4e5ce963f
AARTYOON <aaron_toledo@dlsu.edu.ph>
2023-11-28 22:27:28 +0800

    I think final

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -16,8 +16,6 @@
 :- dynamic grandchild/2.
 :- dynamic grandson/2.
 :- dynamic granddaughter/2.
-:- discontiguous father/2.
-:- discontiguous mother/2.
 
 father(X,Y) :- male(X), parent(X,Y), X\=Y,!.
 mother(X,Y) :- female(X), parent(X,Y), X\=Y,!.
